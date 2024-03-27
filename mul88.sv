89ede42db4485f9136e8c1c57a61e4f626a0e879
huynguyendinhhcmut <151980955+huynguyendinhhcmut@users.noreply.github.com>
2024-03-28 00:33:39 +0700

    Add files via upload

--- mul88.sv
+++ mul88.sv
@@ -1,19 +1,17 @@
-module mul88(
+module mul88 (
 	input logic [7:0] a, b,
 	input logic cin,
 	output logic [15:0] mul
 );
-
 wire [7:0] sum1, sum2, sum3, sum4, sum5, sum6, sum7;
-wire [7:0] carry;
+wire [0:0] carry1, carry2, carry3, carry4, carry5, carry6, carry7;
 
 //layer0
 and (mul[0], a[0], b[0]);
 
-//1st
-FA8b layer1 (
+//layer1
+FA8b fa1 (
 	.sum(sum1),
-	.cin(1'b0),
 	.a0(a[1] & b[0]),
 	.a1(a[2] & b[0]),
 	.a2(a[3] & b[0]),
@@ -22,21 +20,21 @@ FA8b layer1 (
 	.a5(a[6] & b[0]),
 	.a6(a[7] & b[0]),
 	.a7(1'b0),
-	.cout(carry[0]),
 	.b0(a[0] & b[1]),
 	.b1(a[1] & b[1]),
-	.b2(a[1] & b[1]),
+	.b2(a[2] & b[1]),
 	.b3(a[3] & b[1]),
 	.b4(a[4] & b[1]),
 	.b5(a[5] & b[1]),
 	.b6(a[6] & b[1]),
-	.b7(a[7] & b[1])
+	.b7(a[7] & b[1]),
+	.cin(cin),
+	.cout(carry1)
 );
-
-//2nd
-FA8b layer2 (
+	
+//layer2
+FA8b fa2 (
 	.sum(sum2),
-	.cin(1'b0),
 	.a0(sum1[1]),
 	.a1(sum1[2]),
 	.a2(sum1[3]),
@@ -44,22 +42,22 @@ FA8b layer2 (
 	.a4(sum1[5]),
 	.a5(sum1[6]),
 	.a6(sum1[7]),
-	.a7(carry[0]),
-	.cout(carry[1]),
+	.a7(carry1),
 	.b0(a[0] & b[2]),
 	.b1(a[1] & b[2]),
-	.b2(a[1] & b[2]),
+	.b2(a[2] & b[2]),
 	.b3(a[3] & b[2]),
 	.b4(a[4] & b[2]),
 	.b5(a[5] & b[2]),
 	.b6(a[6] & b[2]),
-	.b7(a[7] & b[2])
+	.b7(a[7] & b[2]),
+	.cin(cin),
+	.cout(carry2)
 );
 
-//3rd
-FA8b layer3 (
+//layer3
+FA8b fa3 (
 	.sum(sum3),
-	.cin(1'b0),
 	.a0(sum2[1]),
 	.a1(sum2[2]),
 	.a2(sum2[3]),
@@ -67,22 +65,22 @@ FA8b layer3 (
 	.a4(sum2[5]),
 	.a5(sum2[6]),
 	.a6(sum2[7]),
-	.a7(carry[1]),
-	.cout(carry[2]),
+	.a7(carry2),
 	.b0(a[0] & b[3]),
 	.b1(a[1] & b[3]),
-	.b2(a[1] & b[3]),
+	.b2(a[2] & b[3]),
 	.b3(a[3] & b[3]),
 	.b4(a[4] & b[3]),
 	.b5(a[5] & b[3]),
 	.b6(a[6] & b[3]),
-	.b7(a[7] & b[3])
-);	
+	.b7(a[7] & b[3]),
+	.cin(cin),
+	.cout(carry3)
+);
 
-//4th
-FA8b layer4 (
+//layer4
+FA8b fa4 (
 	.sum(sum4),
-	.cin(1'b0),
 	.a0(sum3[1]),
 	.a1(sum3[2]),
 	.a2(sum3[3]),
@@ -90,22 +88,22 @@ FA8b layer4 (
 	.a4(sum3[5]),
 	.a5(sum3[6]),
 	.a6(sum3[7]),
-	.a7(carry[2]),
-	.cout(carry[3]),
+	.a7(carry3),
 	.b0(a[0] & b[4]),
 	.b1(a[1] & b[4]),
-	.b2(a[1] & b[4]),
+	.b2(a[2] & b[4]),
 	.b3(a[3] & b[4]),
 	.b4(a[4] & b[4]),
 	.b5(a[5] & b[4]),
 	.b6(a[6] & b[4]),
-	.b7(a[7] & b[4])
-);		
+	.b7(a[7] & b[4]),
+	.cin(cin),
+	.cout(carry4)
+);
 
-//5th
-FA8b layer5 (
+//layer5
+FA8b fa5 (
 	.sum(sum5),
-	.cin(1'b0),
 	.a0(sum4[1]),
 	.a1(sum4[2]),
 	.a2(sum4[3]),
@@ -113,22 +111,22 @@ FA8b layer5 (
 	.a4(sum4[5]),
 	.a5(sum4[6]),
 	.a6(sum4[7]),
-	.a7(carry[3]),
-	.cout(carry[4]),
+	.a7(carry4),
 	.b0(a[0] & b[5]),
 	.b1(a[1] & b[5]),
-	.b2(a[1] & b[5]),
+	.b2(a[2] & b[5]),
 	.b3(a[3] & b[5]),
 	.b4(a[4] & b[5]),
 	.b5(a[5] & b[5]),
 	.b6(a[6] & b[5]),
-	.b7(a[7] & b[5])
-);	
+	.b7(a[7] & b[5]),
+	.cin(cin),
+	.cout(carry5)
+);
 
-//6th
-FA8b layer6 (
+//layer6
+FA8b fa6 (
 	.sum(sum6),
-	.cin(1'b0),
 	.a0(sum5[1]),
 	.a1(sum5[2]),
 	.a2(sum5[3]),
@@ -136,22 +134,22 @@ FA8b layer6 (
 	.a4(sum5[5]),
 	.a5(sum5[6]),
 	.a6(sum5[7]),
-	.a7(carry[4]),
-	.cout(carry[5]),
+	.a7(carry5),
 	.b0(a[0] & b[6]),
 	.b1(a[1] & b[6]),
-	.b2(a[1] & b[6]),
+	.b2(a[2] & b[6]),
 	.b3(a[3] & b[6]),
 	.b4(a[4] & b[6]),
 	.b5(a[5] & b[6]),
 	.b6(a[6] & b[6]),
-	.b7(a[7] & b[6])
+	.b7(a[7] & b[6]),
+	.cin(cin),
+	.cout(carry6)
 );
 
-//7th
-FA8b layer7 (
+//layer7
+FA8b fa7 (
 	.sum(sum7),
-	.cin(1'b0),
 	.a0(sum6[1]),
 	.a1(sum6[2]),
 	.a2(sum6[3]),
@@ -159,31 +157,19 @@ FA8b layer7 (
 	.a4(sum6[5]),
 	.a5(sum6[6]),
 	.a6(sum6[7]),
-	.a7(carry[5]),
-	.cout(mul[15]),
+	.a7(carry6),
 	.b0(a[0] & b[7]),
 	.b1(a[1] & b[7]),
-	.b2(a[1] & b[7]),
+	.b2(a[2] & b[7]),
 	.b3(a[3] & b[7]),
 	.b4(a[4] & b[7]),
 	.b5(a[5] & b[7]),
 	.b6(a[6] & b[7]),
-	.b7(a[7] & b[7])
+	.b7(a[7] & b[7]),
+	.cin(cin),
+	.cout(carry7)
 );
 
-assign mul[1] =  sum1[0];
-assign mul[2] =  sum2[0];
-assign mul[3] =  sum3[0];
-assign mul[4] =  sum4[0];
-assign mul[5] =  sum5[0];
-assign mul[6] =  sum6[0];
-assign mul[7] =  sum7[0];
-assign mul[8] =  sum7[1];
-assign mul[9] =  sum7[2];
-assign mul[10] =  sum7[3];
-assign mul[11] =  sum7[4];
-assign mul[12] =  sum7[5];
-assign mul[13] =  sum7[6];
-assign mul[14] =  sum7[7];
+assign mul[15:1] = {carry7, sum7[7:0], sum6[0], sum5[0], sum4[0], sum3[0], sum2[0], sum1[0]};
 
 endmodule
