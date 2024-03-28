3a5df11abb56e636a2b7359ba54e5311853c7b2c
huynguyendinhhcmut <151980955+huynguyendinhhcmut@users.noreply.github.com>
2024-03-28 23:03:34 +0700

    Add files via upload

--- lab1_ex3.sv
+++ lab1_ex3.sv
@@ -1,11 +1,24 @@
 module lab1_ex3 (
 	input logic [0:0] EA, EB, clk, rst,
 	input logic [7:0] a, 
+	output logic [7:0] outled,
 	output logic [15:0] P
 );
 
+logic [1:0] out;
+assign out[0] = EA;
+assign out[1] = EB;
 wire [7:0] A, B;
 wire [15:0] H;
+
+always_comb begin
+	unique case (out)
+		2'b01: outled = A;
+		2'b10: outled = B;
+		2'b11: outled = 8'b00000000;
+		2'b00: outled = 8'b00000000;
+	endcase
+end
 	
 D_FF8b dff8b0 (.clk(clk), .rst(rst), .enable(EA), .data_i(a), .data_o(A));
 D_FF8b dff8b1 (.clk(clk), .rst(rst), .enable(EB), .data_i(a), .data_o(B));
--- wrapper_lab1_ex3.sv
+++ wrapper_lab1_ex3.sv
@@ -4,12 +4,10 @@ module wrapper_lab1_ex3 (
 	output logic [6:0] HEX0, HEX1, HEX2
 );
 
-assign LEDR[9:0] = SW[9:0];
-
 wire [7:0] A;
 wire [15:0] p;
 
-lab1_ex3 lab1ex3 (.EA(SW[9]), .EB(SW[8]), .rst(KEY[0]), .clk(KEY[1]), .a(A), .P(p));
+lab1_ex3 lab1ex3 (.EA(SW[9]), .EB(SW[8]), .rst(KEY[0]), .clk(KEY[1]), .a(A), .P(p), .outled(LEDR[7:0]));
 
 display_hex displayhex0 (.data_in(p[3:0]), .display_out(HEX0));
 display_hex displayhex1 (.data_in(p[7:4]), .display_out(HEX1));
